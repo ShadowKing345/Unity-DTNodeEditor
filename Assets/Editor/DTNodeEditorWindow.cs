20bb5e865eea841d30ca2c1c2b8d97b0135a1f87
Alex <alexandruturcanu915@gmail.com>
2020-02-29 23:21:33 +0300

    Inital Project Commit

---
+++ Assets/Editor/DTNodeEditorWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEditor;
+
+public class DTNodeEditorWindow : EditorWindow
+{
+    [MenuItem("Window/Editor Window")]
+    public static void ShowWindow()
+    {
+        GetWindow<DTNodeEditorWindow>("Example");
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
