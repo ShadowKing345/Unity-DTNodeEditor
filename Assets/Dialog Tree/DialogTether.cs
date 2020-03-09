9c6b4b4f03a26248b792b073dfaa33fa23560b0f
Alexandru Turcanu <alexandruturcanu915@gmail.com>
2020-03-09 18:54:25 +0300

    Altered the documentation.

--- Assets/Dialog Tree/DialogTether.cs
+++ Assets/Dialog Tree/DialogTether.cs
@@ -1,30 +1,43 @@
 ﻿using System;
 
-/**
- * This is the object used to point where the next node is.
- */
+///<summary>This object is used to point where the next node is.</summary>
 [Serializable]
 public class DialogTether
 {
-    //Used if the Dialog Manage is suppose to show a branch. Leave empty to not show. 
+    ///<summary>Used if the Dialog Manage is suppose to show a branch. Leave empty to not show.</summary> 
     public string _text;
-    //The destination Dialog Node.
+    ///<summary>The destination Dialog Node.</summary>
     public BasicDialogNode _destination;
 
+    #region Constructers
+    /// <summary>
+    /// Constructor of <c>DialogTether</c> used to preset the _text value.
+    /// </summary>
+    /// <param name="text">the string to set the _text variable</param>
     public DialogTether(string text)
     {
         _text = text;
     }
     
+    /// <summary>
+    /// Constructor of <c>DialogTether</c> used to preset the _text value and _destination node.
+    /// </summary>
+    /// <param name="text">the string to set the _text variable</param>
+    /// <param name="destination">a <c>BasicDialogNode</c> that will be the destination.</param>
     public DialogTether(string text, BasicDialogNode destination)
     {
         _text = text;
         _destination = destination;
     }
 
+    /// <summary>
+    /// Constructor of <c>DialogTether</c> used to preset the _destination node.
+    /// </summary>
+    /// <param name="destination">a <c>BasicDialogNode</c> that will be the destination.</param>
     public DialogTether(BasicDialogNode destination)
     {
         _text = "";
         _destination = destination;
     }
+    #endregion
 }
--- Assets/Dialog Tree/DialogTree.cs
+++ Assets/Dialog Tree/DialogTree.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 
-/**
- * This is a main tree for the dialog.
- */
+/// <summary>The main container for all the Dialog Nodes.</summary>
 [CreateAssetMenu(menuName = "Dialog/Dialog Tree")]
 public class DialogTree : ScriptableObject
 {
-    //The Starting node.
+    ///<summary>The starting dialog node.</summary>
     public BasicDialogNode _startNode;
-    //This is a list of all the dialog nodes contained in the tree object.
+    ///<summary>This is a list of all the dialog nodes contained in the tree object.</summary>
     public BasicDialogNode[] _nodeReferences; 
 }
